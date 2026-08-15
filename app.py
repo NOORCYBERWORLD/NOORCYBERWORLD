597f7a94098a51935a486f94547e76ee703ff47e
NOORCYBERWORLD <nawajmujawar786@gmail.com>
2026-08-15 17:17:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,26 +17,6 @@ from reportlab.lib import colors
 # PAGE CONFIG
 # ============================================================
 
-st.markdown(
-    """
-    <div class="nc-header">
-
-        <div class="nc-title">
-            NOOR CYBER WORLD
-        </div>
-
-        <div class="nc-main-title">
-            CUSTOMER MANAGEMENT SYSTEM
-        </div>
-
-        <div class="nc-sub">
-            DIGITAL SERVICE • CUSTOMER RECORD • SMART MANAGEMENT
-        </div>
-
-    </div>
-    """,
-    unsafe_allow_html=True
-)
 
 
 # ============================================================
