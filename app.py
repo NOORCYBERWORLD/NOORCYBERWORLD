3ad5d5bd55161a31f1b1ca112c187dcb9ac9d3e6
NOORCYBERWORLD <nawajmujawar786@gmail.com>
2026-08-15 13:18:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,7 @@ def today_ist():
 # ============================================================
 
 WEB_APP_URL = (
-    "https://script.google.com/macros/s/"
-    "AKfycbwSipN_etRHmOKXczikdg1gwzBvksliKCLQ0NYIJX9BbCGcyalc8H14aMTo_mNAbytK"
-    "/exec"
+    "https://script.google.com/macros/s/AKfycbwSipN_etRHmOKXczikdg1gwzBvksliKCLQ0NYIJX9BbCGcyalc8H14aMTo_mNAbytK/exec"
 )
 
 
